1ad868be5cb712f1bed3401b331cf1637c4ddbf4
fahid2002 <fahid23105101323.diu.edu.bd>
2026-05-21 12:42:59 +0600

    2nd

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,29 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "i.pravatar.cc" },
+      { protocol: "https", hostname: "i.ibb.co" },
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+      { protocol: "https", hostname: "avatars.githubusercontent.com" },
+    ],
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/doctors/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/doctors/:path*`,
+      },
+      {
+        source: "/api/appointments/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/appointments/:path*`,
+      },
+      {
+        source: "/api/reviews/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/reviews/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
