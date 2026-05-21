5834a4d02f818eaa929fb0bb2b0264d451fe5f27
fahid2002 <fahid23105101323.diu.edu.bd>
2026-05-21 12:56:11 +0600

    auth-client

---
+++ src/libs/auth-client.js
@@ -0,0 +1,11 @@
+import { createAuthClient } from "better-auth/react";
+
+export const authClient = createAuthClient({
+  baseURL:
+    process.env.NEXT_PUBLIC_BETTER_AUTH_URL ||
+    (typeof window !== "undefined"
+      ? window.location.origin
+      : "https://doc-appoint-client.vercel.app"),
+});
+
+export const { signIn, signOut, signUp, useSession, getSession } = authClient;
